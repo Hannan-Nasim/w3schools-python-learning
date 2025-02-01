24e0c6aea6c5b5321bd6c0b7fadebc911e26ad9a
Hannan-Nasim <hannannasim07@gmail.com>
2025-02-02 01:45:24 +0500

    Day one of learning

---
+++ Python Get Started/02_python_syntax.py
@@ -0,0 +1,35 @@
+# Python Indentation
+
+# Indentation refers to space at the beginning of a line of code
+
+# The indentation in pytho i very important in other languages it is only for readibility
+
+# Python uses indentation to indicate a block of code
+
+# The code below works fine as it has proper indentation 
+if 5>2:
+    print("Five is greater than two.(proper indentation)")
+
+# The commented code below gives error because indentation is not correct. Indentation error it is. 
+# if 5>2:
+# print("Five is greater than two.")
+
+# The number of spaces is upto programmer, the most common is four, but it has to be at least one.
+if 5>2:
+ print("Five is greater than two.(One spaces only)")
+if 5>2:
+    print("Five is greater than two.(Four spaces only)")
+
+# Python Variables 
+# In Python, variables are created when you assign a value to it.
+# Python has no command for declaring a variable
+
+x = 5
+y = "Hello, World!"
+
+# Python has commenting capability for the purpose of in-code documentation
+# Comments start with a #, and  Python will render rest of the line  as a comment.
+
+# This is a comment
+print("The line above is a comment")
+
---
+++ Python Get Started/03_python_comments.py
@@ -0,0 +1,31 @@
+#Comments can be used to explain python code.
+#Comments can be used to make code more readable.
+#Comments can be used to prevent execution when testing code.
+
+# Creating a comment
+# Comment starts with  a #, and python wil ignore them:
+# This is a comment
+print("I have commented everything above")
+
+print("At the end of this line I have placed a comment and pythn will ignore it.") # See thi will not print cool cool man.
+
+# A comment does not have to be the text that explains code, it can also be used to prevent python from executing code.
+#print("This print statement will not be printed")
+
+#Python has no multiline comment
+#We have to insert # for each line
+print("We have used more than one # as we don't have multiline comment in python")
+
+#But but we can use multiline string.
+#Since python ignore string literals taht are not assigned to a variable, you can add a multiline string (triple quotes in your code)
+"""
+This  is a comment
+written in 
+more than just one line
+"""
+print("Multiline comment is used above")
+
+"""
+As long as the string is not assigned to a variable.
+Python will read the code, but then ignore it, and you have made a multiline comment.
+"""
